cde45fc568c12e29e29769e277144c00ec522079
Jonathan Alva <jonathan.alva97@yahoo.com>
2020-03-11 21:57:45 +0700

    Done Pokemon List

--- src/views/Container/PokemonList/PokemonList.jsx
+++ src/views/Container/PokemonList/PokemonList.jsx
@@ -13,20 +13,25 @@ class PokemonList extends Component {
 
         this.state = {
             currentData: [],
+            pageCount: 0,
+            prevButton: 0,
+            nextButton: 0,
+            number1Button: 0,
+            number2Button: 0,
+            number3Button: 0,
+            prevDisable: "",
+            nextDisable: "",
+            active1Button: "",
+            active2Button: "",
+            active3Button: ""
         }
-    }
-    handleClick(e, index) {
-        e.preventDefault();
-        this.setState({
-            currentPage: index
-        });
-    
     }
     componentDidMount() {
         this.props.getInitalData();
-        let page = this.props.match.params.page * 20;
-        API.getPokemonSpeciesOffset(page).then( res=>{
-            res.results.map(async data=>{
+        let pageNumber = parseInt(this.props.match.params.page);
+        let page = pageNumber * 20;
+        API.getPokemonSpeciesOffset(page).then(res=>{
+            res.results.map(data=>{
                 let pokemonData = [];
                 let currentData = [...this.state.currentData];
                 let pokemonId = data.url.split('/')[6];
@@ -45,8 +50,8 @@ class PokemonList extends Component {
                         pokemonData['textColor'] = "white";
                     }
                     else if(pokemonColor == 'gray'){
-                        pokemonData['color'] = "#F4F5F4";
-                        pokemonData['textColor'] = "white";
+                        pokemonData['color'] = "#F5F5F5";
+                        pokemonData['textColor'] = "#818181";
                     }
                     else if(pokemonColor == 'green'){
                         pokemonData['color'] = "#2CDAB1";
@@ -65,7 +70,7 @@ class PokemonList extends Component {
                         pokemonData['textColor'] = "white";
                     }
                     else if(pokemonColor == 'white'){
-                        pokemonData['color'] = "#F5F5F5";
+                        pokemonData['color'] = "white";
                         pokemonData['textColor'] = "#818181";
                     }
                     else if(pokemonColor == 'yellow'){
@@ -83,6 +88,52 @@ class PokemonList extends Component {
                         pokemonData['type1'] = pokemonTypeList[0].type.name.charAt(0).toUpperCase() + pokemonTypeList[0].type.name.slice(1);
                         pokemonData['type2'] = pokemonTypeList[1].type.name.charAt(0).toUpperCase() + pokemonTypeList[1].type.name.slice(1);
                     }
+                    if(r.sprites != null){
+                        this.setState({
+                            pageCount: Math.floor(res.count/20)
+                        },()=>{
+                            if(pageNumber != 0 && pageNumber != this.state.pageCount){
+                                this.setState({
+                                    prevButton: pageNumber-1,
+                                    number1Button: pageNumber-1,
+                                    number2Button: pageNumber,
+                                    number3Button: pageNumber+1,
+                                    nextButton: pageNumber+1,
+                                    prevDisable: "",
+                                    nextDisable: "",
+                                    active1Button: "",
+                                    active2Button: "active",
+                                    active3Button: ""
+                                })
+                            }
+                            else if(pageNumber == 0){
+                                this.setState({
+                                    prevButton: 0,
+                                    number1Button: pageNumber,
+                                    number2Button: pageNumber+1,
+                                    number3Button: pageNumber+2,
+                                    nextButton: pageNumber+1,
+                                    prevDisable: "disabled",
+                                    active1Button: "active",
+                                    active2Button: "",
+                                    active3Button: ""
+                                })
+                            }
+                            else if(pageNumber == this.state.pageCount){
+                                this.setState({
+                                    prevButton: pageNumber-1,
+                                    number1Button: pageNumber-2,
+                                    number2Button: pageNumber-1,
+                                    number3Button: pageNumber,
+                                    nextButton: 0,
+                                    nextDisable: "disabled",
+                                    active1Button: "",
+                                    active2Button: "",
+                                    active3Button: "active"
+                                })
+                            }
+                        })
+                    }
                     pokemonData['form'] = r.sprites;
                     pokemonData['name'] = r.name.charAt(0).toUpperCase() + r.name.slice(1);
                     pokemonData['id'] = pokemonId;
@@ -92,7 +143,6 @@ class PokemonList extends Component {
                     })
                 })
             })
-            
         })
     }
 
@@ -114,13 +164,13 @@ class PokemonList extends Component {
                 <div style={{overflow:"hidden"}}>
                     <div className="container-fluid" style={{ backgroundColor: "#2c2c2c", paddingLeft: "0", paddingRight: "0" }}>
                         <div id="pokemon_list_header">
-                            <div className="p-2" style={{backgroundColor:"rgba(69,148,205,0.9)", width:"50%"}}>
+                            <div className="p-2 content_pokemon_list_header rounded" style={{backgroundColor:"rgba(69,148,205,0.9)"}}>
                                 <TitleComponent
                                     title_middle="POKEMON LIST"
                                 />
                                 <DescriptionComponent
                                     description_text="This is a list of every Pokemon in this world."
-                                    description_text_color="#d1d1d1"
+                                    description_text_color="white"
                                     description_padding_right="0"
                                     fontSize="1.5rem"
                                 />
@@ -129,60 +179,29 @@ class PokemonList extends Component {
                         <div className="row">
                             <div className="col-lg-12 col-md-12 col-sm-x col-xs-x col-x px-3 pt-2 pb-3" style={{ backgroundColor: "#d1d1d1" }}>
                                 <div className="container-fluid">
-                                    {/* <div className="row">
-                                        <div className="col-md-3">
-                                            <Select
-                                                value={selectedOptionType1}
-                                                onChange={this.handleChangePokemonType1}
-                                                options={this.state.optionType1}
-                                            />
-                                        </div>
-                                        <div className="col-md-3">
-                                            <Select
-                                                value={selectedOptionType2}
-                                                onChange={this.handleChangePokemonType2}
-                                                options={this.state.optionType2}
-                                            />
-                                        </div>
-                                        <div className="col-md-3">
-                                            <Select
-                                                value={selectedOptionHabitat}
-                                                onChange={this.handleChangePokemonHabitat}
-                                                options={this.state.optionHabitat}
-                                            />
-                                        </div>
-                                        <div className="col-md-3">
-                                            <div>
-                                                <button className="btn w-50">Search</button>
-                                                <button className="btn w-50">Reset</button>
-                                            </div>
-                                        </div>
-                                    </div> */}
                                     <div className="row mt-5 mb-1">
                                         {
                                             element
                                         }
                                     </div>
-                                    <div className="text-center">
-                                        <div>
-                                            <ul class="pagination"> 
-                                                <li class="page-item"> 
-                                                    <a class="page-link" href="#">Previous</a> 
-                                                </li> 
-                                                <li class="page-item"> 
-                                                    <a class="page-link" href="#">1</a> 
-                                                </li> 
-                                                <li class="page-item"> 
-                                                    <a class="page-link" href="#">2</a> 
-                                                </li> 
-                                                <li class="page-item"> 
-                                                    <a class="page-link" href="#">3</a> 
-                                                </li> 
-                                                <li class="page-item"> 
-                                                    <a class="page-link" href="#">Next</a> 
-                                                </li> 
-                                            </ul> 
-                                        </div>
+                                    <div>
+                                        <ul className="pagination justify-content-center"> 
+                                            <li className={`page-item ${this.state.prevDisable}`}> 
+                                                <a className="page-link" href={`./${this.state.prevButton}`}>Previous</a> 
+                                            </li> 
+                                            <li className={`page-item ${this.state.active1Button}`}> 
+                                                <a className="page-link" href={`./${this.state.number1Button}`}>{this.state.number1Button}</a> 
+                                            </li> 
+                                            <li className={`page-item ${this.state.active2Button}`}> 
+                                                <a className="page-link" href={`./${this.state.number2Button}`}>{this.state.number2Button}</a> 
+                                            </li> 
+                                            <li className={`page-item ${this.state.active3Button}`}> 
+                                                <a className="page-link" href={`./${this.state.number3Button}`}>{this.state.number3Button}</a> 
+                                            </li> 
+                                            <li className={`page-item ${this.state.nextDisable}`}> 
+                                                <a className="page-link" href={`./${this.state.nextButton}`} disabled>Next</a> 
+                                            </li> 
+                                        </ul> 
                                     </div>
                                 </div>
                             </div>
