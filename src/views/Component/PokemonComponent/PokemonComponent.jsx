06a394fc2f6919eb846e78ed43175f0e6937e525
Jonathan Alva <jonathan.alva97@yahoo.com>
2020-03-11 19:57:08 +0700

    Pokemon List Card

--- src/views/Component/PokemonComponent/PokemonComponent.jsx
+++ src/views/Component/PokemonComponent/PokemonComponent.jsx
@@ -14,18 +14,23 @@ class PokemonComponent extends Component{
     render(){
         return(
             <Fragment>
-                <a className="link-url" href="#" style={{color:"#818181"}}>
-                    <div className="img-container">
-                        <img className="img" src={this.props.src}/>
-                        <p className="pokemon-number">#{this.props.number}</p>
-                        <p className="pokemon-habitat">{this.props.habitat}</p>
-                        <p className="pokemon-name">{this.props.pokemon_name}</p>
-                        <div className="row">
-                            <div className="col">
-                                <p className="pokemon-type">{this.props.type1}</p>
+                <a className="link-url" href="#" style={{color:this.props.pokemon_text_color}}>
+                    <div className="img-container" style={{ backgroundColor:this.props.pokemon_color}}>
+                        <div className="row" style={{margin:0}}>
+                            <div className="col-12 col-xs-6 col-md-6 ">
+                                <div className="position-relative h-100">
+                                    <div className="pt-1 pr-1">
+                                        <p className="pokemon-name">{this.props.pokemon_name}</p>
+                                    </div>
+                                    <div className="pokemon-type-box">
+                                        <p className="pokemon-type">{this.props.type1}</p>
+                                        <p className="pokemon-type">{this.props.type2}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="col">
-                                <p className="pokemon-type">{this.props.type2}</p>
+                            <div className="col-12 col-xs-6 col-md-6">
+                                <p className="pokemon-number">#{this.props.number}</p>
+                                <img className="img" src={this.props.src}/>
                             </div>
                         </div>
                     </div>
--- src/views/Container/PokemonList/PokemonList.jsx
+++ src/views/Container/PokemonList/PokemonList.jsx
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
 import './PokemonList.css';
-import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import PokemonComponent from '../../Component/PokemonComponent/PokemonComponent';
 import TitleComponent from '../../Component/TitleComponent/TitleComponent';
 import { connect } from 'react-redux';
 import { getInitalData } from "../../../redux/reducer/globalReducer";
 import API from "../../../service";
 import DescriptionComponent from '../../Component/DescriptionComponent/DescriptionComponent';
-import Select from 'react-select';
 
 class PokemonList extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            // currentPage: 0,
-            // pageSize: 20,
-            // dataCount: 0,
-            // pagesCount: 0,
-            // pokemonType: [],
-            // pokemonHabitat: [],
-            
-            // selectedOptionType1: null,
-            // selectedOptionType2: null,
-            // selectedOptionHabitat: null,
-
-            // optionType1:[],
-            // optionType2:[],
-            // optionHabitat:[],
-
             currentData: [],
-            // currentDetailsData: [],
         }
     }
     handleClick(e, index) {
@@ -42,110 +24,77 @@ class PokemonList extends Component {
     }
     componentDidMount() {
         this.props.getInitalData();
-        
-        // API.getPokemonTypes().then(res=>{
-        //     this.setState({
-        //         pokemonType: res
-        //     },()=>{
-        //         let optionsPokemonType = [];
-        //         this.state.pokemonType.results.map(res=>{
-        //             let data = {
-        //                 value: res.url, label: res.name
-        //             }
-        //             optionsPokemonType = [...optionsPokemonType, data];
-        //             this.setState({
-        //                 optionType1: optionsPokemonType,
-        //                 optionType2: optionsPokemonType,
-        //             })
-        //         })
-                
-        //     })
-        // })
-
-        // API.getPokemonHabitats().then(res=>{
-        //     this.setState({
-        //         pokemonHabitat: res
-        //     },()=>{
-        //         let optionsPokemonHabitat = [];
-        //         this.state.pokemonHabitat.results.map(res=>{
-        //             let data = {
-        //                 value: res.url, label: res.name
-        //             }
-        //             optionsPokemonHabitat = [...optionsPokemonHabitat, data];
-        //             this.setState({
-        //                 optionHabitat: optionsPokemonHabitat
-        //             })
-        //         })
-        //     })
-        // })
-        // API.getPokemonSpecies().then(res=>{
-        //     this.setState({
-        //         dataCount: res.count
-        //     },()=>{
-        //         this.setState({
-        //             pagesCount: this.state.dataCount / this.state.pageSize
-        //         })
-        //     })
-        // })
         let page = this.props.match.params.page * 20;
-
-        // console.log(page)
-        API.getPokemonSpeciesOffset(page).then(res=>{
-            // console.log(res)
-            res.results.map(data=>{
+        API.getPokemonSpeciesOffset(page).then( res=>{
+            res.results.map(async data=>{
                 let pokemonData = [];
                 let currentData = [...this.state.currentData];
-                // console.log(data)
                 let pokemonId = data.url.split('/')[6];
                 API.getPokemonSpeciesData(pokemonId).then(r=>{
-                    console.log(r)
-                    pokemonData['habitat'] = r.habitat['name'];
+                    let pokemonColor = r.color.name;
+                    if(pokemonColor == 'black'){
+                        pokemonData['color'] = "#303943";
+                        pokemonData['textColor'] = "white";
+                    }
+                    else if(pokemonColor == 'blue'){
+                        pokemonData['color'] = "#58abf6";
+                        pokemonData['textColor'] = "white";
+                    }
+                    else if(pokemonColor == 'brown'){
+                        pokemonData['color'] = "#CA8179";
+                        pokemonData['textColor'] = "white";
+                    }
+                    else if(pokemonColor == 'gray'){
+                        pokemonData['color'] = "#F4F5F4";
+                        pokemonData['textColor'] = "white";
+                    }
+                    else if(pokemonColor == 'green'){
+                        pokemonData['color'] = "#2CDAB1";
+                        pokemonData['textColor'] = "white";
+                    }
+                    else if(pokemonColor == 'pink'){
+                        pokemonData['color'] = "#FFB6C1";
+                        pokemonData['textColor'] = "#818181";
+                    }
+                    else if(pokemonColor == 'purple'){
+                        pokemonData['color'] = "#9F5BBA";
+                        pokemonData['textColor'] = "white";
+                    }
+                    else if(pokemonColor == 'red'){
+                        pokemonData['color'] = "#F7786B";
+                        pokemonData['textColor'] = "white";
+                    }
+                    else if(pokemonColor == 'white'){
+                        pokemonData['color'] = "#F5F5F5";
+                        pokemonData['textColor'] = "#818181";
+                    }
+                    else if(pokemonColor == 'yellow'){
+                        pokemonData['color'] = "#FFCE4B";
+                        pokemonData['textColor'] = "white";
+                    }
                 })
                 API.getPokemonData(pokemonId).then(r =>{
                     let pokemonTypeList = r.types;
                     if(pokemonTypeList.length == 1){
-                        pokemonData['type1'] = pokemonTypeList[0].type.name;
+                        pokemonData['type1'] = pokemonTypeList[0].type.name.charAt(0).toUpperCase() + pokemonTypeList[0].type.name.slice(1);
                         pokemonData['type2'] = "";
                     }
                     else{
-                        pokemonData['type1'] = pokemonTypeList[0].type.name;
-                        pokemonData['type2'] = pokemonTypeList[1].type.name;
+                        pokemonData['type1'] = pokemonTypeList[0].type.name.charAt(0).toUpperCase() + pokemonTypeList[0].type.name.slice(1);
+                        pokemonData['type2'] = pokemonTypeList[1].type.name.charAt(0).toUpperCase() + pokemonTypeList[1].type.name.slice(1);
                     }
                     pokemonData['form'] = r.sprites;
-                    pokemonData['name'] = r.name;
+                    pokemonData['name'] = r.name.charAt(0).toUpperCase() + r.name.slice(1);
                     pokemonData['id'] = pokemonId;
                     currentData = [...this.state.currentData, pokemonData];
                     this.setState({
                         currentData: currentData
-                    },()=>{
-                        // console.log(this.state.currentData.habitat, this.state.pokemonData)
                     })
                 })
             })
             
         })
     }
-
-    // handleChangePokemonType1 = selectedOptionType1 => {
-    //     this.setState({
-    //         selectedOptionType1
-    //     })
-    //     // console.log(selectedOptionType1)
-    // };
-
-    // handleChangePokemonType2 = selectedOptionType2 => {
-    //     this.setState({
-    //         selectedOptionType2
-    //     })
-    //     // console.log(selectedOptionType2)
-    // };
-
-    // handleChangePokemonHabitat = selectedOptionHabitat => {
-    //     this.setState({
-    //         selectedOptionHabitat
-    //     })
-    //     // console.log(selectedOptionHabitat)
-    // };
 
     render() {
         const data = this.state.currentData;
@@ -155,76 +104,97 @@ class PokemonList extends Component {
             obj.sort((a,b)=>a.id - b.id);
 
             obj.map((res,i)=>{
-                if(res!=[] && res.name != undefined && res.habitat!=undefined && res.id!=undefined && res.form.front_default!=undefined){
+                if(res!=[] && res.name != undefined && res.id!=undefined && res.form.front_default!=undefined){
                     element = [...element, <div className="col-lg-3 col-md-3 col-sm-4 col-6 mb-4" key={i}>
-                        <PokemonComponent pokemon_name={res.name} habitat={res.habitat} number={res.id} src={res.form.front_default} type1={res.type1} type2={res.type2} />
+                        <PokemonComponent pokemon_color={res.color} pokemon_text_color={res.textColor} pokemon_name={res.name} number={res.id} src={res.form.front_default} type1={res.type1} type2={res.type2} />
                     </div>]
                 }
             })
-        }
-        // const { selectedOptionType1 } = this.state;
-        // const { selectedOptionType2 } = this.state;
-        // const { selectedOptionHabitat } = this.state;
-        return (
-            <div style={{overflow:"hidden"}}>
-                <div className="container-fluid" style={{ backgroundColor: "#2c2c2c", paddingLeft: "0", paddingRight: "0" }}>
-                    <div id="pokemon_list_header">
-                        <div className="p-2" style={{backgroundColor:"rgba(69,148,205,0.6)"}}>
-                            <TitleComponent
-                                title_middle="POKEMON LIST"
-                            />
-                            <DescriptionComponent
-                                description_text="This is a list of every Pokemon in this world."
-                                description_text_color="#d1d1d1"
-                                description_padding_right="55"
-                                fontSize="1.5rem"
-                            />
+            return (
+                <div style={{overflow:"hidden"}}>
+                    <div className="container-fluid" style={{ backgroundColor: "#2c2c2c", paddingLeft: "0", paddingRight: "0" }}>
+                        <div id="pokemon_list_header">
+                            <div className="p-2" style={{backgroundColor:"rgba(69,148,205,0.9)", width:"50%"}}>
+                                <TitleComponent
+                                    title_middle="POKEMON LIST"
+                                />
+                                <DescriptionComponent
+                                    description_text="This is a list of every Pokemon in this world."
+                                    description_text_color="#d1d1d1"
+                                    description_padding_right="0"
+                                    fontSize="1.5rem"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12 col-md-12 col-sm-x col-xs-x col-x px-5 py-5" style={{ backgroundColor: "#d1d1d1" }}>
-                            <div className="container-fluid">
-                                {/* <div className="row">
-                                    <div className="col-md-3">
-                                        <Select
-                                            value={selectedOptionType1}
-                                            onChange={this.handleChangePokemonType1}
-                                            options={this.state.optionType1}
-                                        />
+                        <div className="row">
+                            <div className="col-lg-12 col-md-12 col-sm-x col-xs-x col-x px-3 pt-2 pb-3" style={{ backgroundColor: "#d1d1d1" }}>
+                                <div className="container-fluid">
+                                    {/* <div className="row">
+                                        <div className="col-md-3">
+                                            <Select
+                                                value={selectedOptionType1}
+                                                onChange={this.handleChangePokemonType1}
+                                                options={this.state.optionType1}
+                                            />
+                                        </div>
+                                        <div className="col-md-3">
+                                            <Select
+                                                value={selectedOptionType2}
+                                                onChange={this.handleChangePokemonType2}
+                                                options={this.state.optionType2}
+                                            />
+                                        </div>
+                                        <div className="col-md-3">
+                                            <Select
+                                                value={selectedOptionHabitat}
+                                                onChange={this.handleChangePokemonHabitat}
+                                                options={this.state.optionHabitat}
+                                            />
+                                        </div>
+                                        <div className="col-md-3">
+                                            <div>
+                                                <button className="btn w-50">Search</button>
+                                                <button className="btn w-50">Reset</button>
+                                            </div>
+                                        </div>
+                                    </div> */}
+                                    <div className="row mt-5 mb-1">
+                                        {
+                                            element
+                                        }
                                     </div>
-                                    <div className="col-md-3">
-                                        <Select
-                                            value={selectedOptionType2}
-                                            onChange={this.handleChangePokemonType2}
-                                            options={this.state.optionType2}
-                                        />
-                                    </div>
-                                    <div className="col-md-3">
-                                        <Select
-                                            value={selectedOptionHabitat}
-                                            onChange={this.handleChangePokemonHabitat}
-                                            options={this.state.optionHabitat}
-                                        />
-                                    </div>
-                                    <div className="col-md-3">
+                                    <div className="text-center">
                                         <div>
-                                            <button className="btn w-50">Search</button>
-                                            <button className="btn w-50">Reset</button>
+                                            <ul class="pagination"> 
+                                                <li class="page-item"> 
+                                                    <a class="page-link" href="#">Previous</a> 
+                                                </li> 
+                                                <li class="page-item"> 
+                                                    <a class="page-link" href="#">1</a> 
+                                                </li> 
+                                                <li class="page-item"> 
+                                                    <a class="page-link" href="#">2</a> 
+                                                </li> 
+                                                <li class="page-item"> 
+                                                    <a class="page-link" href="#">3</a> 
+                                                </li> 
+                                                <li class="page-item"> 
+                                                    <a class="page-link" href="#">Next</a> 
+                                                </li> 
+                                            </ul> 
                                         </div>
                                     </div>
-                                </div> */}
-                                <div className="row mt-5">
-                                    {
-                                        element
-                                    }
                                 </div>
-                               
                             </div>
                         </div>
                     </div>
-                </div>
-            </div >
-        )
+                </div >
+            )
+        }
+        else{
+            return false;
+        }
+        
     }
 }
 
