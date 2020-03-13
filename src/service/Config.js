1dae13537490a6c27b7e99dd82fb27d48961f112
Jonathan Alva <jonathan.alva97@yahoo.com>
2020-03-13 19:54:25 +0700

    Try add url API

--- src/service/Config.js
+++ src/service/Config.js
@@ -1,2 +1,3 @@
-export const rootPath = 'http://localhost/pokemon'; // using PMA for backend.. maybe change to other backend later
+// export const rootPath = 'http://localhost/pokemon'; // using PMA for backend.. maybe change to other backend later
 export const onlinePath = 'https://pokeapi.co/api/v2'; // URL for online API
+export const rootPath = 'https://my-pokemon-api.000webhostapp.com';
